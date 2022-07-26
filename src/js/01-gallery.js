3d224a6bd26c303e3d47f3ac48267efd654ce59d
KarbanLVA <iam82@ukr.net>
2022-07-26 14:25:57 +0300

    виконав задачу №1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const createGallertItemsMarkup = galleryItems.map(({ preview, original, description }) =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+).join('');
+gallery.insertAdjacentHTML('beforeend', createGallertItemsMarkup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "botton",
+});
