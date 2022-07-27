57d320929913f60fb96c6937c6b215be1af950b1
KarbanLVA <iam82@ukr.net>
2022-07-27 12:28:46 +0300

    додано - задача №2.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+// console.log(player);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(time) {    
+    const changeTime = time.seconds;
+    localStorage.setItem('videoplayer-current-time', changeTime);  
+    console.log(changeTime);
+};
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+console.log(currentTime);   
+
+player.setCurrentTime(currentTime).then(function (seconds) {});
+
 
